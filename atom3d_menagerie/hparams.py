8a182f69dcae987653537dff3349d6aa8907d945
Kale Kundert <kale@thekunderts.net>
2023-12-05 17:28:10 -0500

    feat: simplify the creation of combinatorial hyperparameter sets

--- atom3d_menagerie/hparams.py
+++ atom3d_menagerie/hparams.py
@@ -1,6 +1,17 @@
 from dataclasses import asdict
+from itertools import product
 from functools import partial
 from collections.abc import Mapping
+
+def make_hparams(factory, **kwargs):
+    hparams = []
+    keys = kwargs.keys()
+
+    for values in product(*kwargs.values()):
+        factory_kwargs = dict(zip(keys, values))
+        hparams.append(factory(**factory_kwargs))
+
+    return hparams
 
 def label_hparams(key, *hparams):
     if isinstance(key, str):
--- tests/test_hparams.py
+++ tests/test_hparams.py
@@ -1,5 +1,22 @@
-from atom3d_menagerie.hparams import label_hparams
+from atom3d_menagerie.hparams import make_hparams, label_hparams
 from dataclasses import dataclass
+
+def test_make_hparams():
+    @dataclass
+    class HParams:
+        x: int
+        y: int
+
+    hparams = make_hparams(HParams, x=[1, 2], y=[3,4,5])
+
+    assert hparams == [
+            HParams(1, 3),
+            HParams(1, 4),
+            HParams(1, 5),
+            HParams(2, 3),
+            HParams(2, 4),
+            HParams(2, 5),
+    ]
 
 def test_label_hparams_str_int():
     assert label_hparams('i_{0}', 1, 2) == {'i_1': 1, 'i_2': 2}
